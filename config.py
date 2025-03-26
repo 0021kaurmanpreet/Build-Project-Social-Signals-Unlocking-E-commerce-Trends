07ceffe403705f67174f884b05046774f7ea4539
Manpreet Kaur <163019958+0021kaurmanpreet@users.noreply.github.com>
2025-03-26 11:15:07 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Database connection details
+USERNAME = "root"        # Username for the database
+PASSWORD = "Avneet2409"  # Password for the database
+HOST = "localhost"       # Database host (localhost for local development)
+DATABASE = "ecommerce"   # Name of the database being accessed
