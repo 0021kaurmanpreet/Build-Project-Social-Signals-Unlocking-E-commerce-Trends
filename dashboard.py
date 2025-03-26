2f1d4175dd5841e9b566fe460fb2cd78021e5ef0
Manpreet Kaur <163019958+0021kaurmanpreet@users.noreply.github.com>
2025-03-26 10:49:27 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ import seaborn as sns
 
 # Database connection details
 USERNAME = "root"        # Username for the database
-PASSWORD = "password"  # Password for the database
+PASSWORD = "pass@ecommerce123"  # Password for the database
 HOST = "localhost"       # Database host (localhost for local development)
 DATABASE = "ecommerce"   # Name of the database being accessed
 
